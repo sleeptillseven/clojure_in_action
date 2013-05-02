2b6bdf30933e9c3afc034e7e22830cdd218256a7
Christoph Jasinski <christoph.jasinski@googlemail.com>
2013-05-02 06:57:50 +0200

    improve comment

--- chapter-1/src/chapter_1/core.clj
+++ chapter-1/src/chapter_1/core.clj
@@ -5,7 +5,7 @@
 ; a simple add function for two operands
 (defn my-addition [operand1 operand2] (+ operand1 operand2))
 
-; users hash map
+; hash map with user's names, credentials and number of pets
 (def users {"kyle" {:password "secretk" :number-pets 2}
             "siva" {:password "secrets" :number-pets 4}
             "rob" {:password "secretr" :number-pets 6}
