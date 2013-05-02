a188ef4032b217519225aa522cf094db625eac4c
Christoph Jasinski <christoph.jasinski@googlemail.com>
2013-05-02 18:04:51 +0200

    Surrender to comment convention

--- chapter-1/src/chapter_1/core.clj
+++ chapter-1/src/chapter_1/core.clj
@@ -2,16 +2,16 @@
 
 (println "*** code from chapter 1 ***")
 
-; a simple add function for two operands
+;; a simple add function for two operands
 (defn my-addition [operand1 operand2] (+ operand1 operand2))
 
-; hash map with user's names, credentials and number of pets
+;; hash map with user's names, credentials and number of pets
 (def users {"kyle" {:password "secretk" :number-pets 2}
             "siva" {:password "secrets" :number-pets 4}
             "rob" {:password "secretr" :number-pets 6}
             "george" {:password "secretg" :number-pets 8}})
 
-; check where user name and password match with local data
+;; check where user name and password match with local data
 (defn check-login [username password]
   (let [actual-password ((users username) :password)]
     (= actual-password password)))
