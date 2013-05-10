07b72f57b73b9c6a427882b6f231da3db87c48fc
Christoph Jasinski <christoph.jasinski@googlemail.com>
2013-05-11 00:10:10 +0200

    Fix wrong reference

--- chapter-2/project.clj
+++ chapter-2/project.clj
@@ -1,4 +1,4 @@
-(defproject chapter-1 "0.1.0-SNAPSHOT"
+(defproject chapter-2 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
